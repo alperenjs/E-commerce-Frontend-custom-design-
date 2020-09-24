ef6fea9d7a51b6ee07fb1e1309c3b8fa4267ee1f
alperen <64660609+alperenkarate@users.noreply.github.com>
2020-09-24 23:54:29 +0300

    Product Detail page is created with HTML SCSS.

--- productDetail.js
+++ productDetail.js
@@ -53,3 +53,26 @@ function load_content(pass) {
 }
 
 load_content(pass);
+
+///IMG GALLERY
+window.onload = gallery;
+//when we load your gallery in browser then gallery function is loaded
+function gallery() {
+  // gallery is the name of function
+  var allimages = document.images;
+  //now we create a variable allimages
+  //and we store all images in this allimages variable
+  for (var i = 0; i < allimages.length; i++) {
+    //now we create a for loop
+    if (allimages[i].id.indexOf("small") > -1) {
+      allimages[i].onclick = imgChanger;
+      //in above line we are adding a listener to all the thumbs stored inside the allimages array on onclick event.
+    }
+  }
+}
+//declaring the imgChanger function
+function imgChanger() {
+  var img = this;
+  var mySrc = img.src;
+  document.getElementById("myPicture").src = mySrc;
+}
