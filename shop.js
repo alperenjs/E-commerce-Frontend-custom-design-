54ff5b0e1c18ee2a06683ddd587fdffeea0d2610
alperen <64660609+alperenkarate@users.noreply.github.com>
2020-09-25 22:49:17 +0300

    products filters and sorting is done.

--- shop.js
+++ shop.js
@@ -28,6 +28,93 @@ function getUsers() {
       });
     });
 }
+//filter for kategori
+function kategori() {
+  let ktgrFilter = event.target.id;
+  //secilen kategoriyi style ediyor
+  let lists = document.querySelectorAll(".fa-square");
+  lists.forEach((node) => {
+    node.classList.remove("fas");
+  });
+  event.currentTarget.childNodes[0].classList.add("fas");
+  //secilen kategoriyi style ediyor
+  window.scrollTo(0, 0);
+  dataShow.innerHTML = "";
+  fetch("products.json")
+    .then((res) => res.json())
+    .then((data) => {
+      var result = data.filter((items) => items.kind == ktgrFilter);
+      result.forEach((product) => {
+        var output = `
+            <div onclick="goTo(this)" id="${
+              product.id
+            }" class="card" style="width: 15rem;">
+             <img class="card-img-top" src="${
+               product.img
+             }" alt="Card image cap">
+             <div class="card-body">
+                 <p class="card-text">${product.title.toUpperCase()}</p>
+                 <p class="card-text price">&#8378; ${product.price} </p>
+             </div>
+         </div>`;
+
+        dataShow.innerHTML += output;
+      });
+    });
+}
+
+//filter for fiyat
+function filterPrice() {
+  let ktgrFilter = event.target.id;
+  let param1 = event.target.getAttribute("data-first");
+  let param2 = event.target.getAttribute("data-second");
+  console.log(param1, param2);
+  //secilen kategoriyi style ediyor
+  let lists = document.querySelectorAll(".fa-square");
+  lists.forEach((node) => {
+    node.classList.remove("fas");
+  });
+  event.currentTarget.childNodes[0].classList.add("fas");
+  //secilen kategoriyi style ediyor
+  dataShow.innerHTML = "";
+  window.scrollTo(0, 0);
+  fetch("products.json")
+    .then((res) => res.json())
+    .then((data) => {
+      var result = data.filter(
+        (items) => items.price >= param1 && items.price <= param2
+      );
+      result.forEach((product) => {
+        var output = `
+            <div onclick="goTo(this)" id="${
+              product.id
+            }" class="card" style="width: 15rem;">
+             <img class="card-img-top" src="${
+               product.img
+             }" alt="Card image cap">
+             <div class="card-body">
+                 <p class="card-text">${product.title.toUpperCase()}</p>
+                 <p class="card-text price">&#8378; ${product.price} </p>
+             </div>
+         </div>`;
+
+        dataShow.innerHTML += output;
+      });
+    });
+}
+
+//filter for beden
+function filterSize() {
+  let ktgrFilter = event.target.id;
+  //secilen kategoriyi style ediyor
+  let lists = document.querySelectorAll(".fa-square");
+  lists.forEach((node) => {
+    node.classList.remove("fas");
+  });
+  event.currentTarget.childNodes[0].classList.add("fas");
+  //secilen kategoriyi style ediyor
+  window.scrollTo(0, 0);
+}
 
 //azalan artan sıralama
 function sort() {
@@ -66,23 +153,6 @@ function sort() {
         dataShow.innerHTML += sortedOutput;
       });
     });
-  // .then((sortedData) =>
-  //   sortedData.forEach((product) => {
-  //     var sortedOutput = `
-  //     <div onclick="goTo(this)" id="${
-  //       product.id
-  //     }" class="card" style="width: 15rem;">
-  //      <img class="card-img-top" src="${product.img}" alt="Card image cap">
-  //      <div class="card-body">
-  //          <p class="card-text">${product.title.toUpperCase()}</p>
-  //          <p class="card-text price">&#8378; ${product.price} </p>
-  //      </div>
-  //  </div>`;
-
-  //     console.log(product.price);
-  //     dataShow.innerHTML += sortedOutput;
-  //   })
-  // );
 }
 //azalan artan sıralama
 
@@ -95,19 +165,6 @@ function goTo(e) {
   window.location.href = "productDetail.html?pass=" + pass;
 }
 
-//tıklanan ürünün detay sayfasını açma
-
-// document
-//   .getElementsByClassName("card")
-//   .addEventListener("click", AdminButtonClick);
-// function AdminButtonClick() {
-//   ad = event.target.id;
-//   // ad = document.getElementById('kullaniciadi').value;
-//   console.log(ad);
-//   window.location.href = "data2.html?ad=" + ad;
-// }
-
-//open and close filters
 function openNav() {
   document.getElementById("filterBar").style.width = "200px";
   document.getElementById("shopSection").style.marginLeft = "200px";
